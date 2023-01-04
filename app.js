a99c9e9c9f965af54e57daad52e6d722cf903c28
Neeraj Kumar <nk4976744@gmail.com>
2023-01-04 18:44:44 +0530

    added cookies and session

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const { authenticate } = require("passport");
+
 const app = express();
 
 
@@ -13,15 +16,26 @@ const app = express();
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret:"Our little secret.",
+    resave:false,
+    saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-
+userSchema.plugin(passportLocalMongoose);
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -32,38 +46,49 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
     res.render("register");
 });
-
+app.get("/secrets",(req,res)=>{
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }else{
+        res.redirect("/login");
+    }
+});
+app.get("/logout",(req,res)=>{
+    req.logout(err=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/");
+        }
+    });
+    
+});
 app.post("/register", (req, res) => {
-    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-        newUser.save((err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("secrets");
-            }
-        });
+    User.register({username:req.body.username},req.body.password,(err,user)=>{
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }else{
+            passport.authenticate("local")(req,res,()=>{
+                res.redirect("/secrets");
+            });
+        }
     });
 });
 app.post("/login", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne({ email: username }, (err, foundUser) => {
-        if (err) {
-            console.log(err);
-        } else {
-            if (foundUser) {
-                bcrypt.compare(password, foundUser.password, (err, result) => {
-                    if (result === true) {
-                        res.render("secrets");
-                    }
-                });
-            }
-        }
+    const user = new User({
+        username:req.body.username,
+        password:req.body.password
     });
+    req.login(user,(err)=>{
+        if(err){
+            console.log(err);
+        }else{
+            passport.authenticate("local")(req,res,()=>{
+                res.redirect("/secrets");
+            });
+        }
+    })
 });
 app.listen(3000, () => {
     console.log("server started at 3000 port");
